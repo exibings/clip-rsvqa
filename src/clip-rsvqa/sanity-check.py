6cca723c13f1372c6d6148a6610bb70c331123ce
Francisco Guedes <guedes.silva.francisco@gmail.com>
2022-07-24 16:45:20 +0100

    added requirements.txt and packages sanity-check

--- src/clip-rsvqa/sanity-check.py
+++ src/clip-rsvqa/sanity-check.py
@@ -1,10 +1,10 @@
 import requests
 import transformers
-#import datasets
+import torch
 import os
+import datasets
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
 
 
 print("all imports working fine!")
