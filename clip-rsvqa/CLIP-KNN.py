d733189cd4a341e3088d9d304db4ea39eaab3140
Francisco Guedes <guedes.silva.francisco@gmail.com>
2023-01-14 18:29:14 +0000

    CLIP KNN evaluation

---
+++ clip-rsvqa/CLIP-KNN.py
@@ -0,0 +1,51 @@
+import H5Datasets
+import torch
+from tqdm.auto import tqdm
+from transformers import CLIPModel
+import json 
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model_pretrain = "flax-community/clip-rsicd-v2"
+model = CLIPModel.from_pretrained(model_pretrain)
+model.eval()
+model.to(device)
+encodings = json.load(open(os.path.join("datasets", "NWPU-Captions", "nwpu_captions_metadata.json"), "r"))
+dataset = H5Datasets.NwpuCaptionsKNN("validation")
+dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
+
+accuracy = {
+    "k=1": 0,
+    "k=3": 0,
+    "k=5": 0,
+    "k=10": 0
+}
+
+progress_bar = tqdm(range(len(dataset_loader)), desc="KNN Evaluation")
+for batch in dataset_loader:
+    model_input = {
+        "pixel_values": batch["pixel_values"].to(device, non_blocking=True),
+        "input_ids": torch.as_tensor(dataset.classes_input_ids).to(device, non_blocking=True),
+        "attention_mask": torch.as_tensor(dataset.classes_attention_mask).to(device, non_blocking=True),
+        "return_dict": True
+    }
+    outputs = model(**model_input)
+    logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1)
+    probs = probs.detach().cpu().tolist()[0]
+    predictions = [(encodings["id2class"][str(idx)], probs[idx]) for idx in range(len(encodings["id2class"]))]
+    predictions.sort(key=lambda prediction_pair: prediction_pair[1], reverse=True)
+    knn = [1, 3, 5, 10]
+    for k in knn:
+        pred_captions_k = [prediction[0] for prediction in predictions][:k]
+        #print(k,":", pred_captions_k)
+        if encodings["id2class"][str(batch["class"].item())] in pred_captions_k:
+            accuracy["k=" + str(k)] += 1
+    progress_bar.update(1)
+progress_bar.close()
+
+print(f"Accuracies for {model_pretrain}:")
+print("\tk=1:", accuracy["k=1"]/len(dataset))
+print("\tk=3:", accuracy["k=3"]/len(dataset))
+print("\tk=5:", accuracy["k=5"]/len(dataset))
+print("\tk=10:", accuracy["k=10"]/len(dataset))
--- clip-rsvqa/H5Datasets.py
+++ clip-rsvqa/H5Datasets.py
@@ -52,7 +52,7 @@ class RsvqaBenDataset(torch.utils.data.Dataset):
         self.dataset = None
         self.split = split
         self.model_type = model_type
-        self.image_processing = utils.feature_extractor        
+        self.image_processing = utils.ImageProcessing(augment_images=False)      
         self.h5_file_name = "rsvqaxben.h5"
         self.h5_file_path = os.path.join(self.folder, self.h5_file_name)
         self.metadata_file_name = "rsvqaxben_metadata.json"
@@ -75,9 +75,9 @@ class RsvqaBenDataset(torch.utils.data.Dataset):
         output["img_id"] = self.dataset[self.split + "/img_id"][idx]
         output["label"] = self.dataset[self.split + "/label"][idx]
         if self.model_type == "baseline":
-            output["pixel_values"] = np.squeeze(self.image_processing(Image.open(os.path.join("datasets", "RSVQAxBEN", "images", f"{output['img_id'] // 2000:03d}", str(output["img_id"])+".jpg")),return_tensors="np", resample=Image.Resampling.BILINEAR).pixel_values)
+            output["pixel_values"] = self.image_processing(Image.open(os.path.join("datasets", "RSVQAxBEN", "images", f"{output['img_id'] // 2000:03d}", str(output["img_id"])+".jpg")))
         elif self.model_type == "patching":
-            output["pixel_values"] = self.image_processing(utils.patchImage(os.path.join("datasets", "RSVQAxBEN", "images", f"{output['img_id'] // 2000:03d}", str(output["img_id"])+".jpg")), return_tensors="np", resample=Image.Resampling.BILINEAR).pixel_values
+            output["pixel_values"] = self.image_processing(utils.patchImage(os.path.join("datasets", "RSVQAxBEN", "images", f"{output['img_id'] // 2000:03d}", str(output["img_id"])+".jpg")))
         return output
 
     def __len__(self):
@@ -88,6 +88,7 @@ class NwpuCaptionsDataset(torch.utils.data.Dataset):
         self.folder = os.path.join("datasets", "NWPU-Captions")
         self.dataset = None
         self.split = split
+        self.augment_images = augment_images
         self.image_processing = utils.ImageProcessing(augment_images)
         self.h5_file_name = file_name
         self.h5_file_path = os.path.join(self.folder,  self.h5_file_name)
@@ -103,14 +104,45 @@ class NwpuCaptionsDataset(torch.utils.data.Dataset):
             self.dataset = h5py.File(self.h5_file_path, 'r')
         output = {}
         output["img_id"] = self.dataset[self.split + "/img_id"][idx]
-        output["image"] = self.dataset[self.split + "/img_id"][idx].decode("utf-8")
+        output["image"] = self.dataset[self.split + "/image"][idx].decode("utf-8")
         output["class"] = self.dataset[self.split + "/class"][idx]
         output["caption"] = self.dataset[self.split + "/caption"][idx].decode("utf-8")
         output["filtered_caption"] = self.dataset[self.split + "/filtered_caption"][idx].decode("utf-8")
         output["sent_id"] = self.dataset[self.split + "/sent_id"][idx]
         output["input_ids"] = self.dataset[self.split + "/input_ids"][idx]
         output["attention_mask"] = self.dataset[self.split + "/attention_mask"][idx]
-        output["pixel_values"] = self.image_processing(Image.open(os.path.join("datasets", "NWPU-Captions", "images", output["img_id"])))
+        if self.augment_images == False:
+            output["pixel_values"] = self.dataset[self.split + "/pixel_values"][output["img_id"]]
+        else:
+            output["pixel_values"] = self.image_processing(Image.open(os.path.join("datasets", "NWPU-Captions", "images", output["img_id"])))
+        return output
+
+    def __len__(self):
+        return self.dataset_len
+
+class NwpuCaptionsKNN(torch.utils.data.Dataset):
+    def __init__(self, split):
+        self.folder = os.path.join("datasets", "NWPU-Captions")
+        self.dataset = None
+        self.split = split
+        self.h5_file_name = "nwpu_captions.h5"
+        self.h5_file_path = os.path.join(self.folder,  self.h5_file_name)
+        with h5py.File(os.path.join(self.folder, self.h5_file_name), 'r') as file:
+            assert len(file[self.split]["img_id"]) == len(file[self.split]["image"]) == len(file[self.split]["class"]) == len(file[self.split]["caption"]) == len(file[self.split]["filtered_caption"]) == len(
+                file[self.split]["sent_id"]) == len(file[self.split]["input_ids"]) == len(file[self.split]["attention_mask"]), "non matching number of entries in .h5 file."
+            self.dataset_len = len(np.unique(file[self.split]["img_id"]))
+            self.num_classes = len(np.unique(file[self.split]["class"]))
+            self.classes_input_ids = np.array([file["/class_sentence_input_ids"][idx] for idx in range(self.num_classes)])
+            self.classes_attention_mask = np.array([file["/class_sentence_attention_mask"][idx] for idx in range(self.num_classes)])
+
+    def __getitem__(self, idx):
+        if self.dataset is None:
+            self.dataset = h5py.File(self.h5_file_path, 'r')
+        output = {}
+        output["img_id"] = self.dataset[self.split + "/img_id"][idx]
+        output["class"] = self.dataset[self.split + "/class"][idx]
+        output["image"] = self.dataset[self.split + "/image"][idx].decode("utf-8")
+        output["pixel_values"] = self.dataset["/pixel_values"][output["img_id"]]
         return output
 
     def __len__(self):
